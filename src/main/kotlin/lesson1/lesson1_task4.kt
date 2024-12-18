7470dd44ae917e0b7b47c197c31bb9175292e93d
veronikagaraeva <veronikaxveronika@yandex.ru>
2024-12-18 17:26:20 +0300

    KO-1-4 printed all variables

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson1
+
+fun main() {
+
+    val length: Long = 40_868_600_000
+    val age: Int = 27
+    val partOfDay: Double = 0.075
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
